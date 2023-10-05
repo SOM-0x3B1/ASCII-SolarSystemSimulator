709f4ede6bf4e51825aba41bb319b87a614e3c66
SOM-0x3B1 <somadagaszkar@gmail.com>
2023-10-06 00:38:17 +0200

    Add vector math

--- src/vector.c
+++ src/vector.c
@@ -1,1 +1,40 @@
 #include "vector.h"
+#include "math.h"
+
+double negyzet(double a){
+    return a * a;
+}
+
+Vektor vektor_osszeAd(Vektor *u, Vektor *v){
+    Vektor w = {u->x + v->x, u->y + v->y};
+    return w;
+}
+
+Vektor vektor_kivon(Vektor *u, Vektor *v){
+    Vektor w = {u->x - v->x, u->y - v->y};
+    return w;
+}
+
+Vektor vektor_skalarSzoroz(Vektor *u, double skalar){
+    Vektor w = {u->x * skalar, u->y * skalar};
+    return w;
+}
+
+Vektor vektor_skalarOszt(Vektor *u, double skalar){
+    Vektor w = {u->x / skalar, u->y / skalar};
+    return w;
+}
+
+Vektor vektor_negal(Vektor *u){
+    Vektor w = {-(u->x), -(u->y)};
+    return w;
+}
+
+double vektor_tavolsag(Vektor *u, Vektor *v){
+    return sqrt(negyzet(u->x - v->x) + negyzet(u->y - v->y));
+}
+
+Pont vektor_toPont(Vektor u){
+    Pont p = {(int)u.x, (int)u.y};
+    return p;
+}
--- src/vector.h
+++ src/vector.h
@@ -1,8 +1,29 @@
-//
-// Created by somad on 2023. 10. 05..
-//
+#ifndef ASCII_SSS_VECTOR_H
+#define ASCII_SSS_VECTOR_H
 
-#ifndef _NHF_VECTOR_H
-#define _NHF_VECTOR_H
+#endif //ASCII_SSS_VECTOR_H
 
-#endif //_NHF_VECTOR_H
+typedef struct Vektor{
+    double x;
+    double y;
+} Vektor;
+
+typedef struct Pont{
+    int x;
+    int y;
+} Pont;
+
+
+Vektor vektor_osszeAd(Vektor *u, Vektor *v);
+
+Vektor vektor_kivon(Vektor *u, Vektor *v);
+
+Vektor vektor_skalarSzoroz(Vektor *u, double skalar);
+
+Vektor vektor_skalarOszt(Vektor *u, double skalar);
+
+Vektor vektor_negal(Vektor *u);
+
+double vektor_tavolsag(Vektor *u, Vektor *v);
+
+Pont vektor_toPont(Vektor u);
