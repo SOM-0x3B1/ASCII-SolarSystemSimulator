72fc9b2859130c87449448493c50034a4f070b48
SOM-0x3B1 <somadagaszkar@gmail.com>
2023-10-06 00:07:24 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "econio/econio.h"
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ src/vector.h
@@ -0,0 +1,8 @@
+//
+// Created by somad on 2023. 10. 05..
+//
+
+#ifndef _NHF_VECTOR_H
+#define _NHF_VECTOR_H
+
+#endif //_NHF_VECTOR_H
